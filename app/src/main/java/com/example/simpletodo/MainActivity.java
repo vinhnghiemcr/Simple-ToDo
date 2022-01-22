89fc5ac780925e6d0273265e56dd0a8efc33761b
Nghiem Truong <vinhnghiemcr@gmail.com>
2022-01-22 17:49:34 -0600

    Implement of Add button

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,9 +34,22 @@ public class MainActivity extends AppCompatActivity {
         items.add("Go to the gym");
         items.add("Have fun!");
 
-        itemsAdapter itemsAdapter = new itemsAdapter(items);
+        final itemsAdapter itemsAdapter = new itemsAdapter(items);
         rvItems.setAdapter(itemsAdapter);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String todoItem = etItem.getText().toString();
+                //Add item to the model
+                items.add(todoItem);
+                //Notify adapter that an item is inserted
+                itemsAdapter.notifyItemInserted(items.size()-1);
+                etItem.setText("");
+                Toast.makeText(getApplicationContext(), "Item was added!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
